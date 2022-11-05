b722e4531256567fb66ff2461758c137fdc8ca0f
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Pyenv installation script

---
+++ pyenv-dev-install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+# export PATH="$HOME/.pyenv/bin:$PATH"
+# eval "$(pyenv init --path)"
+# eval "$(pyenv virtualenv-init -)"
