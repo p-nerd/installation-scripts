a38cc6a5d3c1b3a5cc9b39f0762dca557a8f03ae
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Gradle installation script

---
+++ gradle-dev-install.sh
@@ -0,0 +1,4 @@
+export VERSION=7.5.1
+wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
+sudo ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
