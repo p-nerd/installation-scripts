36f25cb1d8ba470f11d3e689fcf9299206b1f6ab
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Mongodb installation script

---
+++ mongodb-ubuntu-22.04-install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
