2a3ad1901a265873b9237587139c0f813a0d62f9
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Dotnet installation script

---
+++ dotnet-ubuntu-20.04-install.sh
@@ -0,0 +1,8 @@
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt update && \
+sudo apt install -y dotnet-sdk-6.0
+sudo apt update && \
+sudo apt install -y aspnetcore-runtime-6.0
+sudo apt install -y dotnet-runtime-6.0
