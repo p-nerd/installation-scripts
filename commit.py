b908ed7a4313ce6245e900bab66c57317e0b0ec0
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Commit.py installation script

---
+++ commit.py
@@ -0,0 +1,13 @@
+
+import os
+directory = "."
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        name = f.split("-")[0][2:]
+        name = chr(ord(name[0])-32) + name[1:]
+        file = f[2:]
+        print(file, name)
+        os.system(
+            f"git add {file}; git commit -m '{name} installation script'")
