6bd0736cfeeed55aef4632ff157db20711dddbc6
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 22:33:08 +0600

    Typora installation script

---
+++ typora-deb-install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# or run:
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt update
+# install typora
+sudo apt install typora -y
