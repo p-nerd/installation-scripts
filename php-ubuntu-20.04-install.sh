fdf081dd9ee645e341ba4ea58f8bdf4ae987d875
Shihab Mahamud <shihab4t@gmail.com>
2023-05-13 07:50:30 -0400

    Updates

--- php-ubuntu-20.04-install.sh
+++ php-ubuntu-20.04-install.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-sudo apt install php8.0 -y
-
+sudo apt install php8.1 -y
+sudo apt install php-curl php-mbstring php-gd -y
